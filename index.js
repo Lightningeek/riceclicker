34c74b4b2c1000c7ae53ad4d5f6c225e8f7ca329
Yanjia Ji <117481654+Lightningeek@users.noreply.github.com>
2023-03-19 04:42:54 +0000

    just to make sure replit and github are synced up

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const path = require('path');
-
 const app = express();
 const port = 3000;
 
@@ -17,7 +16,6 @@ app.use(session({
   saveUninitialized: true
 }));
 
-// Middleware to check if user is authenticated
 const isAuthenticated = (req, res, next) => {
   const { user } = req.session;
 
@@ -32,7 +30,6 @@ const isAuthenticated = (req, res, next) => {
   }
 };
 
-// Middleware to clear user session on logout
 const clearUserSession = (req, res, next) => {
   if (req.path === '/logout') {
     req.session.user = undefined;
@@ -40,18 +37,14 @@ const clearUserSession = (req, res, next) => {
   next();
 };
 
-// Add middleware to clear user session on logout
 app.use(clearUserSession);
 
-// Set up a route for the CSS file
 app.get('/style.css', (req, res) => {
   res.sendFile(path.join(__dirname, '/style.css'));
 });
 
-// Middleware to check if user is authenticated
 app.use(isAuthenticated);
 
-// Handle GET request for the login page
 app.get('/login', (req, res) => {
   res.render('login');
 });
@@ -60,30 +53,25 @@ app.get('/login', (req, res) => {
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
 
-  // Check if username and password are valid
   if (username === process.env['USERNAME'] && password === process.env['PASSWORD']) {
-    req.session.user = { username }; // Save the user in the session
-    res.redirect('/'); // Redirect to the index page
+    req.session.user = { username }; 
+    res.redirect('/');
   } else {
     res.send('Invalid username or password');
   }
 });
 
-// Handle GET request for the index page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-// Handle GET request for logout page
 app.get('/logout', (req, res) => {
   req.session.user = undefined;
   res.redirect('/login');
 });
 
-// Serve static files from the root directory
 app.use(express.static(path.join(__dirname, '')));
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
